e540251fb25fa25a39b4ca5e0237588c88d890bc
Steven Nemetz <snemetz@hotmail.com>
2015-04-21 16:58:25 -0700

    Improve formatting

--- report-instance-states.sh
+++ report-instance-states.sh
@@ -8,25 +8,24 @@
 #volume_states="creating available attaching in-use deleting error error_deleting backing-up restoring-backup error_restoring error_extending"
 
 instance_states="ACTIVE BUILD DELETED ERROR PAUSED RESCUED RESIZED SHELVED SHELVED_OFFLOADED SHUTOFF SOFT_DELETED STOPPED SUSPENDED"
-power_states="NOSTATE Running" # numbers ?
+power_states="NOSTATE Running Shutdown" # numbers ?
 task_states="deleting None"
-vm_states="active"
-declare -A active_breakdown
+vm_states="active error"
+declare -A breakout
 
 echo "Instance status counts:"
 for Status in $instance_states; do
-  echo "Instance Status: $Status = $(nova list --all-tenants 1 --status=$Status | egrep -v 'ID|[+]' | wc -l)"
-  if [ $Status == 'ACTIVE' ]; then
+  echo -e "\tInstance Status: $Status\t = $(nova list --all-tenants 1 --status=$Status | egrep -v 'ID|[+]' | wc -l)"
+  if [ $Status == 'ACTIVE' -o $Status == 'ERROR' ]; then
     keys=$(nova list --all-tenants 1 --status=$Status --fields name,power_state,task_state,OS-EXT-STS:vm_state | egrep -v 'Power|[+]' | awk '{ print $6"-"$8"-"$10 }')
     for K in $keys; do
-      ((active_breakdown[$K]++))
+      ((breakout[${Status}:${K}]++))
     done
   fi
 done
-echo "Active Instance Breakout:"
-for S in "${!active_breakdown[@]}"; do
-  echo "$S = ${active_breakdown[$S]}"
-done
+echo -e "\nInstance Breakout: Instance Status - Power State - Task State - VM State"
+for S in "${!breakout[@]}"; do
+  echo -e "\t$S\t = ${breakout[$S]}"
+done | sort
+exit
 
-# Need to break down ACTIVE
-#--fields name,power_state,task_state,OS-EXT-STS:vm_state
